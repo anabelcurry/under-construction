a8d91477244f40e2dcbca0203c16e8f200c24692
anabel <anabelcurry@gmail.com>
2025-07-25 14:46:37 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import tvAnimation from './assets/under-constructionTV.gif'
+import resume from './assets/Anabel_Curry_QA engineer_resume.pdf'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div >
+        <img src={tvAnimation} className="tv" alt="tv-animation" />
+      </div>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>New Site Under Construction</h1>
+        <h2>more coming soon!</h2>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <a 
+          href={resume}
+          download="anabel-curry-resume-2025.pdf" 
+        >
+          <button>download resume</button>
+        </a>
+                <a 
+          href='https://www.linkedin.com/in/anabel-curry-28476b176/'
+          target="_blank"
+        >
+          <button>linkedin</button>
+        </a>        <a 
+          href="https://www.mixcloud.com/anabelcurry/" 
+          target='_blank'
+        >
+          <button>music?</button>
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
